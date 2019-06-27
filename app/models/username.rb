126e0645f7cb1a110e16621abde859cb9cd0e697
lsfernandes92 <lsfernandes92@gmail.com>
2019-06-27 19:36:28 -0300

    Create Username model

---
+++ app/models/username.rb
@@ -0,0 +1,10 @@
+class Username < ApplicationRecord
+  validates :username,
+    presence: true,
+    uniqueness: true,
+    length: { minimum: 6 }
+  validates :email, presence: true
+  validates :password,
+    presence: true,
+    length: { minimum: 6 }
+end
