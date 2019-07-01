7eeb92490d66b2d2f877f31c52c7bb22a11c9f70
lsfernandes92 <lsfernandes92@gmail.com>
2019-07-01 13:17:24 -0300

    Change form to create user with helper 'form_tag'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to new_user_path, notice: "User created successfully"
+      redirect_to new_user_path
     else
       render :new
     end
